31ed8df08a14057dbc9581cb9221d52985ce492a
myrosyatsaplap <myrosyatsaplap@ukr.net>
2021-09-24 18:20:54 +0300

    module-1 completed

--- src/module-1/cutStrings/index.js
+++ src/module-1/cutStrings/index.js
@@ -1,1 +1,4 @@
-export const cutStrings = (arr = []) => {};
+export const cutStrings = (arr = []) => {
+  const shortestLength = arr.map(el => el.length).sort((a, b) => a - b)[0];
+  return arr.map(el => el.slice(0, shortestLength));
+};
